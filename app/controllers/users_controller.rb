cb0904e9d396470402615a2adab2a6b25ca8d3b6
Brendan Spinks <bspinks@dius.com.au>
2011-07-14 22:05:00 +1000

    adding gmaps for rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    puts "444444444"
+    @json = User.all.to_gmaps4rails
+  end
+
+  def show
+    puts "#######"
+    puts User.all
+    @json = User.all.to_gmaps4rails
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode
 
+  acts_as_gmappable :latitude => 'latitude', :longitude => 'longitude', :checker => :prevent_geocoding, 
+                  :msg => "Sorry, not even Google could figure out where that is"
+
+  def prevent_geocoding
+    address.blank? || (!lat.blank? && !lng.blank?) 
+  end
+
   def full_address
     "#{street} #{suburb} #{postcode} #{state} #{country}"
   end
